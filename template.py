4fa45959fad14caa147eb0ab072745f68bc35832
Shubham Joshi <indsjos@gmail.com>
2026-05-04 10:33:01 +0530

    template.py

---
+++ template.py
@@ -0,0 +1,48 @@
+from pathlib import Path
+import os
+import logging
+  
+project_name = "Books_Recommender"
+
+list_of_files = [
+     f"{project_name}/__init__.py",
+    f"{project_name}/components/__init__.py",
+    f"{project_name}/components/stage_00_data_ingestion.py",
+    f"{project_name}/components/stage_01_data_validation.py",
+    f"{project_name}/components/stage_02_data_transformation.py",
+    f"{project_name}/components/stage_03_model_trainer.py",
+    f"{project_name}/config/__init__.py",
+    f"{project_name}/config/configuration.py",
+    f"{project_name}/constant/__init__.py",
+    f"{project_name}/entity/__init__.py",
+    f"{project_name}/entity/config_entity.py",
+    f"{project_name}/exception/__init__.py",
+    f"{project_name}/exception/exception_handler.py",
+    f"{project_name}/logger/__init__.py",
+    f"{project_name}/logger/log.py",
+    f"{project_name}/pipeline/__init__.py",
+    f"{project_name}/pipeline/training_pipeline.py",
+    f"{project_name}/utils/__init__.py",
+    f"{project_name}/utils/util.py",
+    "config/config.yaml",
+    ".dockerignore",
+    "app.py",
+    "Dockerfile",
+    "setup.py"
+
+]
+
+for filepath in list_of_files:
+    filepath=Path(filepath)
+    filedir,filename=os.path.split(filepath)
+    
+    if filedir!="":
+        os.makedirs(filedir,exist_ok=True)
+        logging.info(f"Created directory {filedir}")
+    elif (not os.path.exists(filepath)) or (os.path.getsize(filepath)==0):
+        with open(filepath, "w") as f:
+            pass
+            logging.info(f"Created empty file {filepath}")
+    else:
+        logging.info(f"File {filepath} already exists")
+            
